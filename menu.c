58274c1f1f52dc910614e1589e32a2163ba88d3e
laaurarg <100997662+laaurarg@users.noreply.github.com>
2022-04-27 13:16:11 +0200

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,33 @@
+//El proyecto empieza aqui 
+#include <stdio.h>
+
+//vector de estructura 
+
+struct TEmpleado{
+	char nombre[50], apellido[50];
+	int Nexpediente[10];
+}TEmpleado;
+
+int main(){
+	int i=0;
+	struct TEmpelado Empleados[300]; //guardamos el espacio en suficiente para 300 empelados 
+	
+	printf("Registrarse\n");
+	printf("Introduce tu normbre\n Nombre:");
+	scanf("%s", &Empelados[i].nombre);
+	printf("Introduce tu apellido\n Nombre:");
+	scanf("%s", &Empelados[i].apellido);
+	printf("Introduce tu normbre\n Nombre:");
+	scanf("%d", &Empelados[i].Nexpedinte);
+	
+	switch (option){
+	case 1:
+		system("cls");
+            printf ("\n");
+    		printf ("Explicación de la yinkana\n");
+        	printf("Yinkana compuesta por tres fases, inicial, intermedia y final. Debereis superar pruebas para poder pasar de fases, ya que la segunda y tercera fase tienen el requisito de un mínimo de puntos para acceder a ella, si no obteneis dicho numero de puntos inmediatamente sereis eliminados.\n");
+    break;
+    		
+}
+
+}
