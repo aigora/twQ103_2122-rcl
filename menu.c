d67b5b53889376698b89dbe6cc0e9a5b041f2a52
laaurarg <100997662+laaurarg@users.noreply.github.com>
2022-04-27 14:34:08 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -10,24 +10,24 @@ struct TEmpleado{
 
 int main(){
 	int i=0;
-	struct TEmpelado Empleados[300]; //guardamos el espacio en suficiente para 300 empelados 
+	int opcion;
+	struct TEmpleado Empleados[300]; //guardamos el espacio en suficiente para 300 empelados 
 	
 	printf("Registrarse\n");
 	printf("Introduce tu normbre\n Nombre:");
-	scanf("%s", &Empelados[i].nombre);
-	printf("Introduce tu apellido\n Nombre:");
-	scanf("%s", &Empelados[i].apellido);
-	printf("Introduce tu normbre\n Nombre:");
-	scanf("%d", &Empelados[i].Nexpedinte);
+	scanf("%s", &Empleados[i].nombre);
+	printf("Introduce tu apellido\n Apellido:");
+	scanf("%s", &Empleados[i].apellido);
+	printf("Introduce tu numero de expedinte\n Numero de expediente:");
+	scanf("%d", &Empleados[i].Nexpediente);
 	
-	switch (option){
-	case 1:
+		switch (opcion){
+		case 1:
 		system("cls");
             printf ("\n");
     		printf ("Explicación de la yinkana\n");
         	printf("Yinkana compuesta por tres fases, inicial, intermedia y final. Debereis superar pruebas para poder pasar de fases, ya que la segunda y tercera fase tienen el requisito de un mínimo de puntos para acceder a ella, si no obteneis dicho numero de puntos inmediatamente sereis eliminados.\n");
-    break;
-    		
+    break; 
+		}  		
 }
-
-}
+	
