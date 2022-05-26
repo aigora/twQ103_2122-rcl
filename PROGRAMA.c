ee180dfe31ea6f3cd71f696d813a8416856e28f7
laaurarg <100997662+laaurarg@users.noreply.github.com>
2022-05-26 21:55:14 +0200

    Update PROGRAMA.c

--- PROGRAMA.c
+++ PROGRAMA.c
@@ -5,8 +5,25 @@ struct TEmpleado{
 	char nombre[50]; 
 	char apellidos[50];
 	char NombreUsuario[50];
-    char Contrasena[50];
+    int Contrasena[50];
 	int Nexpediente[10];
+	int puntuacion;
+};
+
+struct Tnivel1 {
+    char pregunta1[100];
+    char pregunta2[100];
+    char pregunta3[100];
+};
+
+struct Tnivel2 {
+    char pregunta1[100];
+    char pregunta2[100];
+    char pregunta3[100];
+};
+
+struct Tnivel3 {
+    char pregunta1[100];
 };
 
 void banner() {
@@ -25,115 +42,122 @@ void banner() {
 	printf("\n");
 }
 
-void intro () {
+int intro () {
 	printf("\n\nBienvenidos a RCL:\n");
-	printf("\nEntre los 300 trajadores, has sido seleccionado para obtar a un ascenso.\n");
-	printf("\nLa empresa ha decidido realizar una serie de pruebas, en las que competiras con otro trabajador, para descubrir quien o quienes obtendran el ascenso. \n");
-	printf("\nPara ello, deberas ir superando distintos niveles, cuya dificultad ira aumentando.\n");
-	printf("La yinkana consta de x niveles.\n");
+	printf("\nEntre los 300 trajadores, has sido elegido como empleado del mes.\n");
+	printf("\nLa empresa ha decidido realizar una serie de pruebas, que deberás ir superando para poder conseguir el ascenso. \n");
+	printf("La yinkana consta de 3 niveles.\n");
 	printf("Cada nivel se compone de 3 pruebas, cuyo valor son 3 puntos (1 punto por prueba).\n");
-	printf("\nAl finalizar las pruebas, se realizara un recuento de nivel.\n");
-	printf("En caso de no obtener los puntos solicitados (superar al menos dos pruebas), seras eliminado del juego, proclamandose ganador el otro jugador.\n");
+	printf("\nAl finalizar las pruebas, se realizara un recuento de puntos.\n");
+	printf("En caso de no obtener los puntos solicitados (superar al menos dos pruebas), seras eliminado del juego, sin poder obtener el ascenso.\n");
 	printf("\nEn cualquier momento, tendras la posibilidad de abandonar la yinkana.\n");
-	printf("\nQuienes hayan superado todos los niveles (con las condiciones necesarias), se someteran a un recuento final.\n ");
-	printf("De dicho recuento se obtendra el ganador del ascenso.\n");
-	printf("Existiendo una unica excepcion, si en el recuento final hay empate, dichos jugadores seran ganadores del ascenso.\n");
+	printf("\n El ascenso lo obtendrás en el caso de superar los 3 niveles (cada nivel con su requisito de puntos, es decir, 2 puntos por nivel como mínimo).\n");
 	printf("\n");
-	
-	system ("pause");
-	system ("cls");
+	return;
 }
 
+int preguntas(char respuesta, char opcioncorrecta, int nivel){
+	int puntuacion=0, bien, mal,solucion;
+	switch (nivel) {
+		case 1:
+			bien = 1;
+			mal = 0;
+		break;
 
-int main() {
+		case 2:
+			bien = 1;
+			mal = 0;
+		break;
+
+		case 3:
+			bien = 1;
+			mal = 0;
+		break;
+	}
+	
+if (respuesta=solucion) {
+		printf ("	obtienes %d puntos\n", bien);
+		puntuacion +=bien;
+	}
+	else {
+		printf ("	Respuesta incorrecta\n");
+		puntuacion = mal;
+	}
+
+return puntuacion;
+};
+
+
+int main () {
 	struct TEmpleado empleados[TAM_MAX];
 	int contador;
 	int i;
-	char opcion;
-	
+	int opcion;
+	FILE*ppreguntasnivel1;
+	FILE*ppreguntasNivel2;
 	banner();
-     
-	 
-	 //Se presenta el menu de opciones con un bucle, y que ese bucle no se termine nunca hasta que no se pulse el boton de salir.
-	 while(1) {
-	 	fflush(stdin);
-	 	printf("1 - Registrarse.\n");
-	 	printf("2 - Iniciar Sesion.\n");
-	 	printf("3 - Nivel 1,2,3.\n");
-	 	printf("5 - Salir del juego.\n");
-	 	scanf("%c", &opcion);
-	 	switch(opcion) {
-	 		case '1':
-	 			while(1) {
-	 				fflush(stdin);
-	 				printf("A - Jugador 1.\n");
-	 				printf("B - Jugador 2.\n");
-	 				scanf("%c", &opcion);
-	 			switch(opcion) {
-	 				case 'A': 
-	 				// Se pregunta que jugaor es, si el jugador 1 o el jugador 2: 
-                    printf("Indica el numero de jugador:\n");
-                    scanf("%d", &contador);
-                
-				    // Pedimos los datos del primer empleado1.
-				   for(i=0; i<contador; i++) {
-     	           printf("Introduce tu nombre:\n");
-     	           scanf("%s", empleados[i].nombre);
-     	           printf("Introduce tus apellidos:\n");
-     	           fflush(stdin);
-     	           scanf("%c", &empleados[i].apellidos);
-     	           printf("Introduce tu numero de expediente:\n");
-     	           scanf("%f", &empleados[i].Nexpediente);
-                   }
-				   for (i=0; i<contador; i++) {
-	 				printf ("%s - %c - %f\n", empleados[i].nombre, empleados[i].apellidos, empleados[i].Nexpediente);
-			       }
-			        intro ();
-			        break;
-			    
-			        case 'B':
-			        // Se pregunta que jugaor es, si el jugador 1 o el jugador 2: 
-                    printf("Indica el numero de jugador:\n");
-                    scanf("%d", &contador);
-                
-				    // Pedimos los datos del primer empleado2.
-				    for(i=0; i<contador; i++) {
-     	            printf("Introduce tu nombre:\n");
-     	            scanf("%s", empleados[i].nombre);
-     	            printf("Introduce tus apellidos:\n");
-     	            fflush(stdin);
-     	            scanf("%c", &empleados[i].apellidos);
-     	            printf("Introduce tu numero de expediente:\n");
-     	            scanf("%f", &empleados[i].Nexpediente);
-                    }
-				    for (i=0; i<contador; i++) {
-	 				printf ("%s - %c - %f\n", empleados[i].nombre, empleados[i].apellidos, empleados[i].Nexpediente);
-			        }
-			         intro ();
-				     break;
-				    }
-				    }
-			case '2':{
-				printf("Iniciar Sesion\n Usuario:");
-                scanf("%s", &empleados[i].NombreUsuario);
-                printf("Contrasena:");
-                scanf("%s", &empleados[i].Contrasena);
-                intro ();
-				break;
+	
+	do {
+	printf("MENU DE OPCIONES\n");
+	printf("1. Registrarse.\n");
+	printf("2. Iniciar  sesión.\n");
+	printf("3. Comienza el juego.\n");
+   	printf("4. Salir del juego.\n");
+    
+	printf("OPCION: ");
+	scanf("%d", &opcion);
+	switch(opcion) {
+        case 1:
+		    
+            printf ("\n");
+    		printf ("Registrarse.\n");
+        	for(i=0; i<contador; i++) {
+     	    printf("Introduce tu nombre:\n");
+     	    scanf("%s", empleados[i].nombre);
+     	    printf("Introduce tus apellidos:\n");
+     	    fflush(stdin);
+     	    scanf("%s", &empleados[i].apellidos);
+     	    printf("Introduce tu numero de expediente:\n");
+     	    scanf("%d", &empleados[i].Nexpediente);
+            }
+			for (i=0; i<contador; i++) {
+	 		printf ("%s - %s - %d.\n", empleados[i].nombre, empleados[i].apellidos, empleados[i].Nexpediente);
 			}
-			case '3':{
-				printf("Nivel 1 2 3\n");				
-				break;
+			printf ("\n\n");
+        break;
+        
+       case 2:
+            printf("\n");
+            printf("Iniciar Sesion\n Usuario:");
+            scanf("%s", &empleados[i].NombreUsuario);
+            printf("Contrasena:");
+            scanf("%d", &empleados[i].Contrasena);
+            intro ();
+		break;
+        
+        
+        /*case 3:
+        	printf("\n");
+        	printf("COMIENZA EL JUEGO");
+			preguntasnivel1 = fopen ("preguntasnivel1.txt", "r");
+			for(i=0; i<preguntasnivel1;i++){
+				if(ppreguntasnivel1== NULL){
+					printf("error en la apertura del fichero\n");
+				}
 			}
-		   case '4':{
-		   	printf("Saliendo del juego.\n");
-		   	exit(0);
-		   }
-		default: {
-			printf("La letra introducida es erronea, porfavor introduzca una letra de las mostradas en el menu del juego.\n");
-			break;
+        	preguntasnivelNivel2 = fopen("preguntasNivel2.txt", "r");*/
+        	
+        
+        
 		}
+	
+	
 	}
-  }
-  	return 0;
+	
+	while (opcion!=4);
+	system("cls");
+	printf ("HAS PERIDIDO LA OPORTUNIDAD DE ASCENDER.\n");
+
+return 0;
+
 }
